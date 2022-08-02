4f6de406b18d0fb0b752046f434bcbc4184e401a
Remy GENY <rgeny@e3r8p1.clusters.42paris.fr>
2022-08-02 14:54:01 +0200

    add operator+(fs::path, fs::path)

--- includes/functions/std.hpp
+++ includes/functions/std.hpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 11:28:46 by rgeny             #+#    #+#             */
-/*   Updated: 2022/08/02 11:32:07 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/08/02 11:38:08 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ namespace fnx
 {
 	std::filesystem::path	operator+	(std::filesystem::path	lhs,
 										 char const * rhs);
+	std::filesystem::path	operator+	(std::filesystem::path	lhs,
+										 std::filesystem::path	rhs);
 }
 
 #endif
--- srcs/std/filesystem.cpp
+++ srcs/std/filesystem.cpp
@@ -6,14 +6,21 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 11:27:52 by rgeny             #+#    #+#             */
-/*   Updated: 2022/08/02 11:31:55 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/08/02 11:37:52 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "std.hpp"
 
 std::filesystem::path	fnx::operator+	(std::filesystem::path lhs,
-									 char const * rhs)
+										 char const * rhs)
+{
+	lhs += rhs;
+	return (lhs);
+}
+
+std::filesystem::path	fnx::operator+	(std::filesystem::path lhs,
+										 std::filesystem::path rhs)
 {
 	lhs += rhs;
 	return (lhs);
